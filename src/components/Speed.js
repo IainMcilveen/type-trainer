ed072840d6331d4a5f557ec1b265f1fec709919b
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-12-25 16:46:28 -0400

    work

--- src/components/Speed.js
+++ src/components/Speed.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Speed(){
+
+    let [text, setText] = useState("");
+    let [errors, setErrors] = useState(0);
+    let [words, setWords] = useState(0);
+    let [char, setChar] = useState(0);
+
+    function updateText(text){
+        setText(text);
+        console.log(findNext(text,char));
+    }
+
+    function findNext(text, start){
+        let next = (text.substring(start,text.length)).indexOf(" ");
+        if(next !== -1){
+            return start + (text.substring(start,text.length)).indexOf(" ");
+        }else{
+            return text.length;
+        }
+    }
+
     return(
         <div className="Speed-main">
-            <h1>Speed </h1>
+            <h1>Speed</h1>
+            <p className="test-text">This is the paragraph for which people will be tested, hopefully they don't suck at typing.</p>
+            <input type="text" onChange={e => updateText(e.target.value)} />
         </div>
     );
 }
