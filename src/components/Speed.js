fd1beb29f6137f7e5d77b3b7fadd925a57701c0c
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2021-01-03 22:34:22 -0400

    thought of a better way to do it

--- src/components/Speed.js
+++ src/components/Speed.js
@@ -1,48 +1,31 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 const parse = require('html-react-parser');
 
 function Speed(props){
 
     //text hooks
-    let [text, setText] = useState("");
+    let [curText, setCurText] = useState("");
+    let [totText, setTotText] = useState("");
+    let [correct, setCorrect] = useState(true);
     let [para, setPara] = useState(props.paragraph);
     
     //hooks for calculations 
-    let [error, setError] = useState(-1);
-    let [words, setWords] = useState(0);
-    let [char, setChar] = useState(0);
+    let [charAmt, setCharAmt] = useState(0);
 
     //function used for updating the text box as well as the paragraph
     //the user reads from
-    function updateText(text){
-        setText(text);
+    useEffect(() => {
+        updateText();
+    });
 
-        if(error != -1){
-            
-        }
-
-        if(text.substring(text.length-1,text.length) !== props.paragraph.charAt(char)){
-            console.log(text.substring(text.length-1,text.length), props.paragraph.charAt(char))
-            setError(char);
-        }else{
-            setError(-1);
-            setChar(char + 1);
-        }
-        setPara(paraStyling(props.paragraph));
+    function updateText() {
+        console.log("curText: "+curText);
+        console.log("totText: "+totText);
+        console.log("charAmt: "+charAmt);
+        console.log("correct: "+correct);
     }
 
-    //add all of the styling to the paragraph 
-    function paraStyling(text,error){
-        //underline the word which is currently being typed
-        let html = '<p>'+(text).substring(0,char)+'<u>'+(text).substring(char,findNext(text,char))+'</u>'+(text).substring(findNext(text,char),text.length)+'</p>'
-        //highlight in red the incorrect character
-        if(error !== -1){
-            html = html.substring(0,error) + '<red>' + html.substring(0,error+1) + '</red>' + html.substring(error+1,html.length);
-        }
-
-        return html
-    }
-
+    /*
     //finds the character location of the next space in the paragraph
     //if there are none left it returns the number of characters left in the paragraph 
     function findNext(text, start){
@@ -52,13 +35,22 @@ function Speed(props){
         }else{
             return text.length;
         }
-    }
+    }*/
 
     return(
         <div className="Speed-main">
             <h1>Speed</h1>
             <div className="Speed-para">{para}</div><br/>
-            <input type="text" onChange={e => updateText(e.target.value)} />
+            <input type="text" onChange={(event) => {
+                let temp = event.target.value;
+                if(temp.charAt(temp.length-1) !== props.paragraph.charAt(charAmt+1)){
+                    setCorrect(false);
+                }else{
+                    setCorrect(true);
+                    setCharAmt(charAmt+1);
+                }
+                setCurText(temp)
+                setTotText(totText + temp.charAt(temp.length-1))}} />
         </div>
     );
 }
